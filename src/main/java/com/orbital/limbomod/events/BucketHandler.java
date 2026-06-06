658956cf187b3428ee0a0832845b6d50dc80a0c4
Orb4t3l <trevducket@gmail.com>
2026-06-05 23:31:40 -0700

    lol i forgot about this mod

--- src/main/java/com/orbital/limbomod/events/BucketHandler.java
+++ src/main/java/com/orbital/limbomod/events/BucketHandler.java
@@ -71,7 +71,7 @@ public class BucketHandler {
 
         held.shrink(1);
         if (held.isEmpty()) player.setItemInHand(event.getHand(), ItemStack.EMPTY);
-        player.inventoryMenu.broadcastChanges();
+        player.inventoryMenu.broadcastFullState();
 
         spawnDisplay(serverLevel, player, resultBucket);
     }
@@ -99,7 +99,7 @@ public class BucketHandler {
 
         held.shrink(1);
         if (held.isEmpty()) player.setItemInHand(event.getHand(), ItemStack.EMPTY);
-        player.inventoryMenu.broadcastChanges();
+        player.inventoryMenu.broadcastFullState();
 
         spawnDisplay(serverLevel, player, resultBucket);
     }
